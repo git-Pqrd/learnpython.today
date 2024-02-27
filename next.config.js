233a29d1b13b585701d539f7a8039e363558ba66
PIQUARD <git-Pqrd@users.noreply.github.com>
2024-02-27 21:06:20 +0000

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,6 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     output: 'export',
+    assetPrefix: "https://s3.eu-central-1.amazonaws.com/learnpython.today/",
     trailingSlash: true,
 }
 
