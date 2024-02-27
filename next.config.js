859b7f0ce445b7915d91b280a92011d78c031bd6
PIQUARD <git-Pqrd@users.noreply.github.com>
2024-02-27 19:50:55 +0000

    Update next.config.js with the asset prefix of my bucket

--- next.config.js
+++ next.config.js
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     output: 'export',
-    assetPrefix: ".",
+    assetPrefix: "https://s3.eu-central-1.amazonaws.com/learnpython.today/",
 }
 
 module.exports = nextConfig
