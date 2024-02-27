1d589f2a929116d88b33433e7b989ea5020b769d
PIQUARD <git-Pqrd@users.noreply.github.com>
2024-02-27 20:49:27 +0000

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     output: 'export',
-    assetPrefix: "https://learnpython.today/",
+    assetPrefix: "/",
 }
 
 module.exports = nextConfig
