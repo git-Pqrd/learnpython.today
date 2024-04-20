235a82f4f68d9fc9f7e096f0fb1c53986e936406
francois <francois.piquard11@gmail.com>
2024-04-20 19:50:27 +0100

    Fixing the router and the layout problem of the game card

--- components/game-card-component.tsx
+++ components/game-card-component.tsx
@@ -1,10 +1,9 @@
-'use client'
+"use client";
 
 import * as React from "react";
 import { Game } from "@/types/game";
 import { TagComponent } from "@/components/tag-component";
 import ControllerIcon from "@/components/ui/controller-icon";
-import { useRouter } from "next/navigation";
 
 import {
   Card,
@@ -15,34 +14,30 @@ import {
 } from "@/components/ui/card";
 
 export function GameCard(props: { game: Game }) {
-  const router = useRouter();
   const article = props.game;
   if (!article) return;
 
-  const handleClick = () => {
-    router.push(`/games/${article.href}`);
-  };
-
   return (
     <Card
-      onClick={handleClick}
       className={`${article.starred ? "shadow-lg shadow-green-500/50" : ""}
-      max-w-full w-full md:w-[450px] mr-4 my-4 flex flex-col justify-between bg-background cursor-pointer`}
+      max-w-full w-full md:w-[450px] my-1 md:mr-4 md:my-4 flex flex-col justify-between bg-background cursor-pointer`}
     >
-      <CardHeader>
-        <CardTitle className="truncate pb-1 flex">
-          <ControllerIcon/> {article.title}
-        </CardTitle>
-        <div className="flex">
-          {article.tags.map((tag) => (
-            <TagComponent key={tag.text} tag={tag} />
-          ))}
-        </div>
-      </CardHeader>
-      <CardContent>{article.synopsis}</CardContent>
-      <CardFooter className="flex cursor-pointer justify-end">
-        Let's Play! &rarr;
-      </CardFooter>
+      <a href={`/games/${article.href}`} className="hover:translate-y-1">
+        <CardHeader>
+          <CardTitle className="truncate pb-1 flex max-w-full">
+            <ControllerIcon /> {article.title}
+          </CardTitle>
+          <div className="flex">
+            {article.tags.map((tag) => (
+              <TagComponent key={tag.text} tag={tag} />
+            ))}
+          </div>
+        </CardHeader>
+        <CardContent>{article.synopsis}</CardContent>
+        <CardFooter className="flex cursor-pointer justify-end">
+          Let's Play! &rarr;
+        </CardFooter>
+      </a>
     </Card>
   );
 }
